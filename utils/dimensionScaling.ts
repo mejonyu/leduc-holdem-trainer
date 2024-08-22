a3449ab059bd61e7d445694dbe05b206a12fb4dd
Jonathan Yu <jonyu02@gmail.com>
2024-08-21 21:03:24 -0400

    Modify dimensions to have a fixed aspect ratio (for iPad compatibility)

--- utils/dimensionScaling.ts
+++ utils/dimensionScaling.ts
@@ -1,11 +1,17 @@
 import { Dimensions } from "react-native";
 
-export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
-  Dimensions.get("window");
+let { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 // Original dev done for iPhone 15 Pro Max.
 const IPHONE_15_PRO_MAX_WIDTH = 430;
 const IPHONE_15_PRO_MAX_HEIGHT = 932;
+
+const IPHONE_15_WIDTH_TO_HEIGHT = IPHONE_15_PRO_MAX_WIDTH /
+  IPHONE_15_PRO_MAX_HEIGHT;
+
+SCREEN_WIDTH = SCREEN_HEIGHT * IPHONE_15_WIDTH_TO_HEIGHT;
+
+export { SCREEN_HEIGHT, SCREEN_WIDTH };
 
 export const scaleHeight = (size: number) => {
   return (size / IPHONE_15_PRO_MAX_HEIGHT) * SCREEN_HEIGHT;
